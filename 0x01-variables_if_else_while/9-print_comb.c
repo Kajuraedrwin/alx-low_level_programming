9622ca7fc3c94e4d63c1753281b0bfbf52d77cf8
Kajuraedrwin <kajuraedrwin@gmail.com>
2023-03-17 03:54:21 -0700

    finale

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,15 @@
 int main(void)
 {
 int i;
-for (i = 0; i < 10; i++)
+for (i = '0'; i <= '9'; i++)
 {
-putchar(i + '0');
-if (i != 9)
+putchar(d);
+if (i != '9')
 {
 putchar(',');
 putchar(' ');
 }
 }
+putchar('\n'
 return (0);
 }
