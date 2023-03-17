759bd679b84becd1916b16c436c56f8f015470b1
Kajuraedrwin <kajuraedrwin@gmail.com>
2023-03-16 19:19:01 -0700

    updated puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
-#include <stdio.h>
+#include <stdio.>
+/**
+ * main-Entry
+ * short descriptions
+ * return Always 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
