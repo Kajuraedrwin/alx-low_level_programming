40505cb2f80df4bc1a3429069251fb2dbe771e67
Kajuraedrwin <kajuraedrwin@gmail.com>
2023-03-16 19:03:46 -0700

    Add 4 puts.c source code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-/**
- * Printing outputs eg
- * Programming is like building a multilingual puzzle 
- */
 int main(void)
 {
-	puts(""\Programming is like building a multilingual puzzle");
-	return 0;
+    puts(""\Programming is like building a multilingual puzzle");
+    return 0;
 }
