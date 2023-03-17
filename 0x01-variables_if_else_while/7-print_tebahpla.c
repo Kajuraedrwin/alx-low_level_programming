75f04e086fa4d34d485c5e1be9b9675c3c339f2b
Kajuraedrwin <kajuraedrwin@gmail.com>
2023-03-17 02:40:46 -0700

    Update 7-print_tebahpla source code

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Entry
- * Return: Always 0
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-int i = 0;
-char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
-while (i < 27)
-{
-putchar(alphabet[25 - i]);
-i++;
-}
-putchar('\n');
-return (0);
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
